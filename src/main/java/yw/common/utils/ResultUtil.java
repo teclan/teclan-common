c7820eb3e0aacfe95b7b23c7747607443e4c4661
tanbingjian <tanbingjian@qq.com>
2018-08-29 11:01:48 +0800

    返回工具类支持分页字段

--- src/main/java/yw/common/utils/ResultUtil.java
+++ src/main/java/yw/common/utils/ResultUtil.java
@@ -7,41 +7,62 @@ public class ResultUtil {
 	/**
 	 * 构造返回JSON
 	 * 
-	 * @param code 返回代码，遵循 html 状态码
+	 * @param code    返回代码，遵循 html 状态码
 	 * @param message 消息摘要，一般为操作结果（成功或失败）
 	 * @return
 	 */
-	public static JSONObject getResponse(String code, String message ) {
-		return getResponse(code, message, null,null);
+	public static JSONObject getResponse(String code, String message) {
+		return getResponse(code, message, null, null);
 	}
-	
+
 	/**
 	 * 构造返回JSON
 	 * 
-	 * @param code 返回代码，遵循 html 状态码
-	 * @param message 消息摘要，一般为操作结果（成功或失败）
+	 * @param code      返回代码，遵循 html 状态码
+	 * @param message   消息摘要，一般为操作结果（成功或失败）
 	 * @param exception 异常原因
 	 * @return
 	 */
 	public static JSONObject getResponse(String code, String message, String exception) {
 		return getResponse(code, message, exception, null);
 	}
-	
+
 	/**
 	 * 构造返回JSON
 	 * 
-	 * @param code 返回代码，遵循 html 状态码
-	 * @param message 消息摘要，一般为操作结果（成功或失败）
+	 * @param code      返回代码，遵循 html 状态码
+	 * @param message   消息摘要，一般为操作结果（成功或失败）
 	 * @param exception 异常原因
-	 * @param object 响应的数据内容，例如查询等操作
+	 * @param object    响应的数据内容，例如查询等操作
 	 * @return
 	 */
-	public static JSONObject getResponse(String code, String message, String exception,Object object) {
+	public static JSONObject getResponse(String code, String message, String exception, Object data) {
 		JSONObject result = new JSONObject();
 		result.put("code", code);
 		result.put("message", message);
 		result.put("exception", exception);
-		result.put("data", Objects.isNull(object)?"":object);
+		result.put("data", Objects.isNull(data) ? "" : data);
+		return result;
+	}
+
+	/**
+	 * 构造返回JSON
+	 * 
+	 * @param code      返回代码，遵循 html 状态码
+	 * @param message   消息摘要，一般为操作结果（成功或失败）
+	 * @param exception 异常原因
+	 * @param data      响应的数据内容，例如查询等操作
+	 * @param pageInfo  分页信息
+	 * @return
+	 */
+	public static JSONObject getResponse(String code, String message, String exception, Object data,
+			JSONObject pageInfo) {
+		JSONObject result = new JSONObject();
+		result.put("code", code);
+		result.put("message", message);
+		result.put("exception", exception);
+		result.put("data", Objects.isNull(data) ? "" : data);
+		result.put("pageInfo", Objects.isNull(pageInfo) ? "" : pageInfo);
 		return result;
 	}
 
